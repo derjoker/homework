490bbde97668068c4f6e61cd8bfc75e434fe3fb6
Feng Ya <fengya04@126.com>
2016-07-24 16:13:08 +0800

    twitter share text

--- FCC/Random_Quote_Machine/quote.js
+++ FCC/Random_Quote_Machine/quote.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   var newQuote = function() {
     var next = Math.floor(Math.random() * (quotes.length));
     $('#quote-text').html(quotes[next]);
+    $('a#share-twitter').attr('href', 'https://twitter.com/intent/tweet?text=' + $('#quote-text').text() + ' - Taylor Swift @taylorswift13');
   };
 
   newQuote();
