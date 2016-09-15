f080e26e9c15ef18bbd5dbb0ad41d527cf2fb537
Feng Ya <fengya04@126.com>
2016-09-15 13:07:55 +0800

    color

--- FCC/Simon_Game/simon.js
+++ FCC/Simon_Game/simon.js
@@ -1,5 +1,5 @@
 
 $(document).ready(function() {
-  $('td > div').eq(3).css({'background-color': 'red'});
+  // $('td > div').eq(3).css({'background-color': 'red'});
   // $('audio').eq(4).trigger('play');
 });
